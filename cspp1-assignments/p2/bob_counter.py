add2c36742673d4e66bc020394b96998708626db
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-02 18:16:45 +0530

    " -> 5 of 5 passed. pylint: 9.33/10 "

--- cspp1-assignments/p2/bob_counter.py
+++ cspp1-assignments/p2/bob_counter.py
@@ -7,21 +7,21 @@ then your program should print
 Number of times bob occurs is: 2'''
 
 def main():
-    s_int = input()
     ''' here we are finding the no of times the word
     bob has occured in a given string
     '''
+    s_int = input()
     x_i = 0
     y_i = 1
     z_i = 2
-    count_i = 0
+    count_a = 0
     while z_i <= len(s_int):
         if s_int[x_i] == 'b' and s_int[y_i] == 'o' and s_int[z_i] == 'b':
-            count_i += 1
-    x_i += 1
-    y_i += 1
-    z_i += 1
-print(count_i)
+            count_a += 1
+        x_i += 1
+        y_i += 1
+        z_i += 1
+    print(count_a)
     # the input string is in s
     # remove pass and start your code here
     
