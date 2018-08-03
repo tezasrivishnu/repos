77e33d3be99e92f6c004a1f7974c813771bed47a
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-03 15:23:04 +0530

    " -> 0 of 3 passed. pylint: 0.91/10 "

--- cspp1-assignments/m5/p4/square_root_newtonrapson.py
+++ cspp1-assignments/m5/p4/square_root_newtonrapson.py
@@ -10,19 +10,18 @@
 # input: 49
 # output: 6.999999999999991
 '''
-
 def main():
-	'''
-	here we are finding the square root of a number using
-	newton-rampson method
-	'''
-	s = int(input())
-	epsilon = 0.01
-	guess = s/2.0
-	nguess = 0
-	while (guess*guess-s) >= epsilon and guess <= s:
-		nguess += 1
-		guess = guess - (((guess**2) - s)/(2*guess))
-	print(guess)
+    '''
+    here we are finding the square root of a number using
+    newton-rampson method
+    '''
+    s_in = int(input())
+    epsilon_c = 0.01
+    guess_g = s/2.0
+    nguess_g = 0
+    while (guess_g*guess_g-s_in) >= epsilon_c and guess_g <= s_in:
+        nguess_g += 1
+        guess_g = guess_g - (((guess_g**2) - s)/(2*guess_g))
+    print(guess_g)
 if __name__ == "__main__":
-	main()
+    main()
