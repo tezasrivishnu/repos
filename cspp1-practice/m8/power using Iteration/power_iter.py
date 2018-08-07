1439813cd6f6e8d969662efc264046f9acb570dc
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-07 11:47:09 +0530

    power itr

---
+++ cspp1-practice/m8/power using Iteration/power_iter.py
@@ -0,0 +1,33 @@
+# Exercise: PowerIter
+# Write a Python function, iterPower(base, exp), that takes in two numbers and returns the base^(exp) of given base and exp.
+
+# This function takes in two numbers and returns one number.
+
+
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    res = base
+    if exp == 0:
+         return 1
+    else:
+        for _ in range(1,exp):
+            res *= base
+    return res
+    
+
+    
+
+
+def main():
+
+    data = input()
+    data = data.split()
+    print(iterPower(float(data[0]),int(data[1]))) 
+
+if __name__== "__main__":
+    main()
