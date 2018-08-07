15aa9d1d08afddab50a7478be7af34b6d3cbc306
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-07 15:17:01 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1-assignments/m8/p1/assignment1.py
+++ cspp1-assignments/m8/p1/assignment1.py
@@ -5,21 +5,20 @@ in one number and returns the factorial of given number.
 
 # This function takes in one number and returns one number.
 '''
-def factorial(n):
+def factorial(n_n):
     '''
     n is positive Integer
 
     returns: a positive integer, the factorial of n.
     '''
-    if n <= 1:
+    if n_n <= 1:
         return 1
-    else:
-        return(n*factorial(n-1))
+    return n_n*factorial(n_n-1)
 def main():
     '''
     giving input for finding a factorial of input
     '''
-    a = input()
-    print(factorial(int(a)))
+    a_n = input()
+    print(factorial(int(a_n)))
 if __name__ == "__main__":
     main()
