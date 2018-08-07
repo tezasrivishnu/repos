9ede49a78cc047a4aba331dd0cf975ec829b8e48
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-07 17:22:21 +0530

    first commit

--- cspp1-practice/m8/GCD using Iteration/gcd_iter.py
+++ cspp1-practice/m8/GCD using Iteration/gcd_iter.py
@@ -1,30 +1,29 @@
+'''
 # Exercise: GCDIter
-# Write a Python function, gcdIter(a, b), that takes in two numbers and returns the GCD(a,b) of given a and b.
+# Write a Python function, gcdIter(a, b), that takes in two numbers and
+returns the GCD(a,b) of given a and b.
 
 # This function takes in two numbers and returns one number.
-
-
-def gcdIter(a, b):
+'''
+def gcd_iter(a_i, b_i):
     '''
     a, b: positive integers
-    
     returns: a positive integer, the greatest common divisor of a & b.
     '''
     # Your code here
-    g = 1
-    c = min(a,b)
-    for i in range (c,0,-1):
-        if (a % i == 0) and (b % i == 0):
-            g = i
+    #g_i = 1
+    c_i = min(a_i, b_i)
+    for i_i in range(c_i, 0, -1):
+        if (a_i % i_i == 0) and (b_i % i_i == 0):
+           # g_i = i_i
             break
-    return i
-
-
-
+    return i_i
 def main():
+    '''
+    giving the input to find the gcd
+    '''
     data = input()
     data = data.split()
-    print(gcdIter(int(data[0]),int(data[1]))) 
-
-if __name__== "__main__":
+    print(gcd_iter(int(data[0]), int(data[1])))
+if __name__ == "__main__":
     main()
