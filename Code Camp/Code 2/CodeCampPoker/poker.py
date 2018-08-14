65924eca99ce4ec723745fff863fb8eeb586c2e8
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-14 12:50:01 +0530

    " -> 3 of 3 passed. pylint: 9.80/10 "

--- Code Camp/Code 2/CodeCampPoker/poker.py
+++ Code Camp/Code 2/CodeCampPoker/poker.py
@@ -110,7 +110,6 @@ def poker(hands):
     # hand_rank is a function passed to max
     # hand_rank takes a hand and returns its rank
     # max uses the rank returned by hand_rank and returns the best hand
-     
     return max(hands, key=hand_rank)
 if __name__ == "__main__":
     # read the number of test cases
