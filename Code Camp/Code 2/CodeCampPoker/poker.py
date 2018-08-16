918133b49b0af594bbd9530d308054e3d5c6fb21
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-16 11:30:20 +0530

    " -> 16 of 19 passed. pylint: 9.80/10 "

--- Code Camp/Code 2/CodeCampPoker/poker.py
+++ Code Camp/Code 2/CodeCampPoker/poker.py
@@ -163,8 +163,7 @@ def hand_rank(hand):
         return 6
     if is_straight(hand):
         return 5
-    else:
-        return is_highcard(hand)
+    return 0
     # By now you should have seen the way a card is represented.
     # If you haven't then go the main or poker function and print the hands
     # Each card is coded as a 2 character string. Example Kind of Hearts is KH
