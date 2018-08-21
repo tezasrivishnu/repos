f972cca09124398358a43c3e344dd472ece3e3ac
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-21 20:42:53 +0530

    " -> 4 of 5 passed. pylint: 6.59/10 "

--- cspp1-assignments/m14/p1/assignment1.py
+++ cspp1-assignments/m14/p1/assignment1.py
@@ -69,17 +69,14 @@ WORDLIST_FILENAME = 'words.txt'
 class Message(object):
     def __init__(self, text):
         '''
-        Initializes a Message object
-                
+        Initializes a Message object      
         text (string): the message's text
-
         a Message object has two attributes:
             self.message_text (string, determined by input text)
             self.valid_words (list, determined using helper function load_words
         '''
         self.message_text = text
         self.valid_words = load_words(WORDLIST_FILENAME)
-
     def get_message_text(self):
         '''
         Used to safely access self.message_text outside of the class
@@ -87,7 +84,6 @@ class Message(object):
         Returns: self.message_text
         '''
         return self.message_text
-
     def get_valid_words(self):
         '''
         Used to safely access a copy of self.valid_words outside of the class
@@ -95,7 +91,6 @@ class Message(object):
         Returns: a COPY of self.valid_words
         '''
         return self.valid_words[:]
-
     def build_shift_dict(self, shift):
         '''
         Creates a dictionary that can be used to apply a cipher to a letter.
@@ -103,37 +98,28 @@ class Message(object):
         character shifted down the alphabet by the input shift. The dictionary
         should have 52 keys of all the uppercase letters and all the lowercase
         letters only.        
-        
         shift (integer): the amount by which to shift every letter of the 
         alphabet. 0 <= shift < 26
-
         Returns: a dictionary mapping a letter (string) to 
                  another letter (string). 
         '''
-
         lower_key = list(string.ascii_lowercase)
         lower_value = list(string.ascii_lowercase)
         shift_lower_value = lower_value[shift:] + lower_value[:shift]
-        
         upper_key = list(string.ascii_uppercase)                 
         upper_value = list(string.ascii_uppercase)
         shift_upper_value = upper_value[shift:] + upper_value[:shift]
-
         full_key = lower_key + upper_key
         full_value = shift_lower_value + shift_upper_value
-
         self.shift_dict = dict(zip(full_key, full_value))
         return self.shift_dict
-
     def apply_shift(self, shift):
         '''
         Applies the Caesar Cipher to self.message_text with the input shift.
         Creates a new string that is self.message_text shifted down the
         alphabet by some number of characters determined by the input shift        
-        
         shift (integer): the shift with which to encrypt the message.
         0 <= shift < 26
-
         Returns: the message text (string) in which every character is shifted
              down the alphabet by the input shift
         '''
@@ -144,7 +130,6 @@ class Message(object):
             else:
                 new.append(word)
         return ''.join(new)
-
 def main():
     '''
         Function to handle testcases
