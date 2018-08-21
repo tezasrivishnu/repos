3ce117c41bd3941543dd34225dae7788969e8d07
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-21 20:44:57 +0530

    " -> 4 of 5 passed. pylint: 8.29/10 "

--- cspp1-assignments/m14/p1/assignment1.py
+++ cspp1-assignments/m14/p1/assignment1.py
@@ -4,7 +4,6 @@ Problem 1 - Build the Shift Dictionary and Apply Shift
 # The Message class contains methods that could be used to apply a
 # cipher to a string, either to encrypt or to decrypt a message
 # (since for Caesar codes this is the same action).
-
 # In the next two questions, you will fill in the methods of the
 # Message class found in ps6.py according to the specifications in the
 # docstrings. The methods in the Message class already filled in are:
@@ -13,7 +12,6 @@ Problem 1 - Build the Shift Dictionary and Apply Shift
 # The getter method get_valid_words(self), notice that this one
 # returns a copy of self.valid_words to prevent someone from mutating the
 # original list.
-
 # In this problem, you will fill in two methods:
 # Fill in the build_shift_dict(self, shift) method of the Message class.
 # Be sure that your dictionary includes both lower and upper case
@@ -21,26 +19,21 @@ Problem 1 - Build the Shift Dictionary and Apply Shift
 # uppercase version are lower and upper case instances of the
 # same letter. What this means is that if the original letter is "a" and
 # its shifted value is "c", the letter "A" should shift to the letter "C".
-
 # If you are unfamiliar with the ordering or characters of the English alphabet,
 # we will be following the letter ordering displayed by
 # string.ascii_lowercase and string.ascii_uppercase:
-
 # >>> import string
 # >>> print(string.ascii_lowercase)
 # abcdefghijklmnopqrstuvwxyz
 # >>> print(string.ascii_uppercase)
 # ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
 # A reminder from the introduction page - characters such as the space character,
 # commas, periods, exclamation points, etc will not be encrypted by this cipher
 # Basically, all the characters within string.punctuation, plus the space (' ')
 # and all numerical characters (0 - 9) found in string.digits.
-
 # Fill in the apply_shift(self, shift) method of the Message class.
 # You may find it easier to use build_shift_dict(self, shift).
 # Remember that spaces and punctuation should not be changed by the cipher.
-
 # Helper code
 '''
 def load_words(file_name):
@@ -53,7 +46,6 @@ def load_words(file_name):
     Depending on the size of the word list, this function may
     take a while to finish.
     '''
-
     # inFile: file
     in_file = open(file_name, 'r')
     # line: string
@@ -62,10 +54,8 @@ def load_words(file_name):
     word_list = line.split()
     in_file.close()
     return word_list
-
 WORDLIST_FILENAME = 'words.txt'
 # Helper code End
-
 class Message(object):
     def __init__(self, text):
         '''
@@ -80,14 +70,12 @@ class Message(object):
     def get_message_text(self):
         '''
         Used to safely access self.message_text outside of the class
-        
         Returns: self.message_text
         '''
         return self.message_text
     def get_valid_words(self):
         '''
         Used to safely access a copy of self.valid_words outside of the class
-        
         Returns: a COPY of self.valid_words
         '''
         return self.valid_words[:]
@@ -137,6 +125,5 @@ def main():
     data = Message(input())
     data.get_message_text()
     print(data.apply_shift(int(input())))
-
 if __name__ == "__main__":
     main()
