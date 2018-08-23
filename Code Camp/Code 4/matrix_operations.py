789691cc25108e4e3e27115e3c2133fc5cc168ca
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-23 13:36:13 +0530

    " -> 7 of 8 passed. pylint: 8.51/10 "

--- Code Camp/Code 4/matrix_operations.py
+++ Code Camp/Code 4/matrix_operations.py
@@ -12,21 +12,22 @@ def mult_matrix(m_1, m_2):
     if len(m_1) != len(m_2[0]):
         print("Error: Matrix shapes invalid for mult")
         return
-    mul_matrix = []
-    for _ in range(len(m_1)):
-        tem_matrix = []
-        for _ in range(len(m_2[0])):
-        #for z in range(len(b)):
-            tem_matrix.append(0)
-        mul_matrix.append(tem_matrix)
-    for row_x in range(len(m_1)):
-        for column_y in range(len(m_2[0])):
-            for row_y in range(len(m_2)):
-                # print(row_x, column_y)
-                # print(row_x, row_y)
-                # print(row_y, column_y)
-                mul_matrix[row_x][column_y] += m_1[row_x][row_y] * m_2[row_y][column_y]
-    return mul_matrix
+    else:
+        mul_matrix = []
+        for _ in range(len(m_1)):
+            tem_matrix = []
+            for _ in range(len(m_2[0])):
+            #for z in range(len(b)):
+                tem_matrix.append(0)
+            mul_matrix.append(tem_matrix)
+        for row_x in range(len(m_1)):
+            for column_y in range(len(m_2[0])):
+                for row_y in range(len(m_2)):
+                    # print(row_x, column_y)
+                    # print(row_x, row_y)
+                    # print(row_y, column_y)
+                    mul_matrix[row_x][column_y] += m_1[row_x][row_y] * m_2[row_y][column_y]
+        return mul_matrix
 
 def add_matrix(m_1, m_2):
     '''
@@ -39,17 +40,18 @@ def add_matrix(m_1, m_2):
     if len(m_1) != len(m_2):
         print("Error: Matrix shapes invalid for addition")
         return
-    addition_matrix = []
-    for _ in range(len(m_1)):
-        temp_matrix = []
-        for _ in range(len(m_2[0])):
-        #for z in range(len(b)):
-            temp_matrix.append(0)
-        addition_matrix.append(temp_matrix)
-    for row in range(len(m_1)):
-        for column in range(len(m_2[0])):
-            addition_matrix[row][column] += m_1[row][column] + m_2[row][column]
-    return addition_matrix
+    else:
+        addition_matrix = []
+        for _ in range(len(m_1)):
+            temp_matrix = []
+            for _ in range(len(m_2[0])):
+            #for z in range(len(b)):
+                temp_matrix.append(0)
+            addition_matrix.append(temp_matrix)
+        for row in range(len(m_1)):
+            for column in range(len(m_2[0])):
+                addition_matrix[row][column] += m_1[row][column] + m_2[row][column]
+        return addition_matrix
 def read_matrix():
     '''
         read the matrix dimensions from input
