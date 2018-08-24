4f8a06894fd97f9508fc1827f4e5fcaed7326a37
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-24 09:36:14 +0530

    " -> 0 of 14 passed. pylint: 0/0 "

---
+++ Code Camp/Code 5/tictactoe.py
@@ -0,0 +1,52 @@
+def mat(matrix):
+    temp = 0
+    if (matrix[0][0] == matrix[1][0] == matrix[2][0] == "x"
+        or matrix[0][1] == matrix[1][1] == matrix[2][1] == "x"
+        or matrix[0][2] == matrix[1][2] == matrix[2][2] == "x"
+        or matrix[0][0] == matrix[0][1] == matrix[0][2] == "x"
+        or matrix[1][0] == matrix[1][1] == matrix[1][2] == "x"
+        or matrix[2][0] == matrix[2][1] == matrix[2][2] == "x"):
+        temp = 1
+    elif (matrix[0][0] == matrix[1][0] == matrix[2][0] == "o"
+        or matrix[0][1] == matrix[1][1] == matrix[2][1] == "o"
+        or matrix[0][2] == matrix[1][2] == matrix[2][2] == "o"
+        or matrix[0][0] == matrix[0][1] == matrix[0][2] == "o"
+        or matrix[1][0] == matrix[1][1] == matrix[1][2] == "o"
+        or matrix[2][0] == matrix[2][1] == matrix[2][2] == "o"):
+        temp = 2
+    if (matrix[0][0] == matrix[1][1] == matrix[2][2] == "x"
+        or matrix[0][2] == matrix[1][1] == matrix[2][0] == "x"):
+        temp = 1
+    elif (matrix[0][0] == matrix[1][1] == matrix[2][2] == "o"
+        or matrix[0][2] == matrix[1][1] == matrix[2][0] == "o"):
+        temp = 2
+    if temp == 1:
+        return 'x'
+    elif temp == 2:
+        return 'o'
+    elif if temp == 0:
+        return 'draw'
+
+def main():
+    matrix = []
+    flag = 0
+    count_x = 0
+    count_o = 0
+    for i in range(3):
+        matrix.append(input().split())
+    for i in range(3):
+        for j in range(3):
+            if matrix[i][j] == 'x' or matrix[i][j] == 'o':
+                flag = 1
+            else:
+                flag = 0
+            if matrix[i][j] == 'x':
+                count_x += 1
+            else:
+                count_o += 1
+    if flag == 0 or count_x > 5 or count_o > 5:
+        print("invalid input")
+    else:
+        print(mat(matrix))
+if __name__ == '__main__':
+    main()       
