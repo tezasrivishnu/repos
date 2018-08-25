76b6c9ab4a82aa7980f55bf2ee7f4015f9612f56
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-08-25 12:08:10 +0530

    " -> 3 of 4 passed. pylint: 8.89/10 "

--- cspp1-assignments/m22/assignment5/frequency_graph.py
+++ cspp1-assignments/m22/assignment5/frequency_graph.py
@@ -13,7 +13,7 @@ def frequency_graph(dictionary):
     le_ = len(lis_)
     for key in range(le_):
         le_1 = len(str(dictionary[lis_[key]]))
-        for values in range(le_1):
+        for _ in range(le_1):
             if dictionary[lis_[key]] == 1:
                 lis__.append('#')
             if dictionary[lis_[key]] == 2:
@@ -22,6 +22,9 @@ def frequency_graph(dictionary):
         print(str(lis_[_]) + " - " + str(lis__[_]))
 
 def main():
+    '''
+    input
+    '''
     dictionary = eval(input())
     frequency_graph(dictionary)
 
