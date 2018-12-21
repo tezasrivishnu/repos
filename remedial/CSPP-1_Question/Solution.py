ce0284fe77a83a304544bc98ec739fa4ca450236
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-12-21 12:09:00 +0530

    "cspp1_m10_a1_1: 13/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

--- remedial/CSPP-1_Question/Solution.py
+++ remedial/CSPP-1_Question/Solution.py
@@ -32,63 +32,58 @@ def main():
                         print(res)
 def sudoku(lis,i,j):
     tem = []
-    hor = hori(lis,i,j)
-    ver = veri(lis,i,j)
-    if len(ver) != len(set(ver)) or len(hor) != len(set(hor)):
-        return "Invalid Sudoku:Duplicate values"
-    else:
-        for k in range(len(lis)):
-            tem.append(lis[k][j])
-        for m in range(len(lis[0])):
-            tem.append(lis[i][m])
-        if (i >= 0 and i <= 2) and (j >= 0 and j <= 2):
-            for x in range(0,3):
-                for y in range(0,3):
-                    tem.append(lis[x][y])
-        elif (i >= 0 and i <= 2) and (j >= 3 and j <= 5):
-            for x in range(0,3):
-                for y in range(3,6):
-                    tem.append(lis[x][y])
-        elif (i >= 0 and i <= 2) and (j >= 6 and j <= 8):
-            for x in range(0,3):
-                for y in range(6,9):
-                    tem.append(lis[x][y])
-        elif (i >= 3 and i <= 5) and (j >= 0 and j <= 2):
-            for x in range(3,6):
-                for y in range(0,3):
-                    tem.append(lis[x][y])
-        elif (i >= 3 and i <= 5) and (j >= 3 and j <= 5):
-            for x in range(3,6):
-                for y in range(3,6):
-                    tem.append(lis[x][y])
-        elif (i >= 3 and i <= 5) and (j >= 6 and j <= 8):
-            for x in range(3,6):
-                for y in range(6,9):
-                    tem.append(lis[x][y])
-        elif (i >= 6 and i <= 8) and (j >= 0 and j <= 2):
-            for x in range(6,9):
-                for y in range(0,3):
-                    tem.append(lis[x][y])
-        elif (i >= 6 and i <= 8) and (j >= 3 and j <= 5):
-            for x in range(6,9):
-                for y in range(3,6):
-                    tem.append(lis[x][y])
-        elif (i >= 6 and i <= 8) and (j >= 6 and j <= 8):
-            for x in range(6,9):
-                for y in range(6,9):
-                    tem.append(lis[x][y])
-        string = ''.join(tem)
-        string = string.replace(".", "")
-        string = list(string)
-        inti = list(map(int,string))
-        whole = [1,2,3,4,5,6,7,8,9]
-        numbers = []
-        for h in range(len(whole)):
-            if whole[h] not in inti:
-                numbers.append(whole[h])
-        string1 = list(map(str,numbers))
-        string1 = ''.join(string1)
-        return string1
+    for k in range(len(lis)):
+        tem.append(lis[k][j])
+    for m in range(len(lis[0])):
+        tem.append(lis[i][m])
+    if (i >= 0 and i <= 2) and (j >= 0 and j <= 2):
+        for x in range(0,3):
+            for y in range(0,3):
+                tem.append(lis[x][y])
+    elif (i >= 0 and i <= 2) and (j >= 3 and j <= 5):
+        for x in range(0,3):
+            for y in range(3,6):
+                tem.append(lis[x][y])
+    elif (i >= 0 and i <= 2) and (j >= 6 and j <= 8):
+        for x in range(0,3):
+            for y in range(6,9):
+                tem.append(lis[x][y])
+    elif (i >= 3 and i <= 5) and (j >= 0 and j <= 2):
+        for x in range(3,6):
+            for y in range(0,3):
+                tem.append(lis[x][y])
+    elif (i >= 3 and i <= 5) and (j >= 3 and j <= 5):
+        for x in range(3,6):
+            for y in range(3,6):
+                tem.append(lis[x][y])
+    elif (i >= 3 and i <= 5) and (j >= 6 and j <= 8):
+        for x in range(3,6):
+            for y in range(6,9):
+                tem.append(lis[x][y])
+    elif (i >= 6 and i <= 8) and (j >= 0 and j <= 2):
+        for x in range(6,9):
+            for y in range(0,3):
+                tem.append(lis[x][y])
+    elif (i >= 6 and i <= 8) and (j >= 3 and j <= 5):
+        for x in range(6,9):
+            for y in range(3,6):
+                tem.append(lis[x][y])
+    elif (i >= 6 and i <= 8) and (j >= 6 and j <= 8):
+        for x in range(6,9):
+            for y in range(6,9):
+                tem.append(lis[x][y])
+    string = ''.join(tem)
+    string = string.replace(".", "")
+    string = list(string)
+    inti = list(map(int,string))
+    whole = [1,2,3,4,5,6,7,8,9]
+    numbers = []
+    for h in range(len(whole)):
+        if whole[h] not in inti:
+            numbers.append(whole[h])
+    string1 = list(map(str,numbers))
+    string1 = ''.join(string1)
+    return string1
 def notthere(temp):
     # num = []
     # horve = list(set(horve))
